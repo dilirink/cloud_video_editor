98ce769e07723f7c1e4c9036b5426981c4459916
Fumycat <fumycat@gmail.com>
2020-06-30 10:13:44 +0700

    rewrite

--- static/index.js
+++ static/index.js
@@ -1,24 +1,53 @@
+var p = "";
+var r = 0;
+var interval = null;
+
+function whoAsked() {
+    var f = (r == 1) ? ".mp3" : ".tar";
+    f = p + f;
+    $.ajax({
+        url: '/data/' + f,
+        success: function(data) {
+            // maybe need some changes
+            console.log("ready");
+            $('body').html('<a href="${f}">Файл готов</a>');
+            clearInterval(interval);
+        }
+    });
+}
+
+
 $(document).ready(function(){
-    // button id
-    $("#button").click(function(){
+    $("#btn").click(function(){
 
         var fd = new FormData();
-        // input id
         var files = $('#file')[0].files[0];
         fd.append('file', files);
-
+        p = $("input").val();
+        if (Boolean(p)) {
+            fd.append('name', p);
+        } else {
+            alert("Выберите имя выходного файла!");
+            return;
+        }
+        var radioValue = $("input[name='param']:checked").val();
+        if (radioValue) {
+            r = radioValue;
+            fd.append('forma', radioValue);
+        } else {
+            alert("Выберите что сделать с файлом!");
+            return;
+        }
+         
         $.ajax({
-            url: 'upload',
+            url: '/',
             type: 'post',
             data: fd,
             contentType: false,
             processData: false,
             success: function(response){
-                if (response != 0) {
-                    // TODO redirect
-                } else {
-                    // error 
-                }
+                $('body').html("<p>Loading...</p>");
+                interval = setInterval(whoAsked, 1000);
             },
         });
     });
